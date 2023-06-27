2a4a1d370bffc8fd364fac31a7c0017b58f29b36
d3viniti <119727268+d3viniti@users.noreply.github.com>
2023-06-27 10:38:10 -0700

    added css styling to search results container

--- src/ components/SearchResults/SearchResults.jsx
+++ src/ components/SearchResults/SearchResults.jsx
@@ -3,7 +3,7 @@ import './SearchResults.css'
 import { useNavigate } from 'react-router-dom'
 import noImage from '../../assets/no-image.svg.png'
 
-function SearchResults({movie}) {
+function SearchResults({movie, setQuery}) {
     //create state for navigate
     const navigate = useNavigate();
     //create a state for backup img if it fails to load, we set to false so it's not there by default
@@ -16,7 +16,7 @@ function SearchResults({movie}) {
 
   return (
     <div className='search-results-item' onClick={handleNavigation}>
-      <img className='result-img' src={imageError ? noImage : `https://image.tmdb.org/t/p/w500{movie.backdrop_path`}
+      <img className='result-img' src={imageError ? noImage :`https://image.tmdb.org/t/p/w500${movie?.backdrop_path}`}
       //if there's no image, set image to no-img
       onError = {() => setImageError(true)} alt="No Image"/>
       <p>{movie?.title}</p>
